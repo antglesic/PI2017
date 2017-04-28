1c00b709f567a6e419ca2ea859fdd8315473bfdb
Silvio Đurić <sildjuric@foi.hr>
2017-04-28 22:59:11 +0200

    Forma prijave i forma kreiranja narudzbenice
    Dodana je forma za prijavu te forma za kreiranje narudzbenica od strane
    vlasnika

---
+++ PI_Projekt_SiM/PI_Projekt_SiM/Vlasnik_narudzbenica.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace PI_Projekt_SiM
+{
+    partial class Vlasnik_narudzbenica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.primkeBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.zahtjevniceBtn = new System.Windows.Forms.Button();
+            this.racuniOtpremniceBtn = new System.Windows.Forms.Button();
+            this.skladisteBtn = new System.Windows.Forms.Button();
+            this.naruciBtn = new System.Windows.Forms.Button();
+            this.djelatniciBtn = new System.Windows.Forms.Button();
+            this.porukeBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dodajBtn = new System.Windows.Forms.Button();
+            this.popisSirovina = new System.Windows.Forms.ListBox();
+            this.kreirajBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // primkeBtn
+            // 
+            this.primkeBtn.Location = new System.Drawing.Point(36, 377);
+            this.primkeBtn.Name = "primkeBtn";
+            this.primkeBtn.Size = new System.Drawing.Size(162, 49);
+            this.primkeBtn.TabIndex = 6;
+            this.primkeBtn.Text = "Primke";
+            this.primkeBtn.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.kreirajBtn);
+            this.groupBox1.Controls.Add(this.popisSirovina);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(220, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(706, 545);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Vlasnik";
+            // 
+            // zahtjevniceBtn
+            // 
+            this.zahtjevniceBtn.Location = new System.Drawing.Point(36, 307);
+            this.zahtjevniceBtn.Name = "zahtjevniceBtn";
+            this.zahtjevniceBtn.Size = new System.Drawing.Size(162, 49);
+            this.zahtjevniceBtn.TabIndex = 8;
+            this.zahtjevniceBtn.Text = "Zahtjevnice";
+            this.zahtjevniceBtn.UseVisualStyleBackColor = true;
+            // 
+            // racuniOtpremniceBtn
+            // 
+            this.racuniOtpremniceBtn.Location = new System.Drawing.Point(36, 235);
+            this.racuniOtpremniceBtn.Name = "racuniOtpremniceBtn";
+            this.racuniOtpremniceBtn.Size = new System.Drawing.Size(162, 49);
+            this.racuniOtpremniceBtn.TabIndex = 9;
+            this.racuniOtpremniceBtn.Text = "Racuni-otpremnice";
+            this.racuniOtpremniceBtn.UseVisualStyleBackColor = true;
+            // 
+            // skladisteBtn
+            // 
+            this.skladisteBtn.Location = new System.Drawing.Point(36, 164);
+            this.skladisteBtn.Name = "skladisteBtn";
+            this.skladisteBtn.Size = new System.Drawing.Size(162, 49);
+            this.skladisteBtn.TabIndex = 10;
+            this.skladisteBtn.Text = "Skladiste";
+            this.skladisteBtn.UseVisualStyleBackColor = true;
+            // 
+            // naruciBtn
+            // 
+            this.naruciBtn.Location = new System.Drawing.Point(36, 93);
+            this.naruciBtn.Name = "naruciBtn";
+            this.naruciBtn.Size = new System.Drawing.Size(162, 49);
+            this.naruciBtn.TabIndex = 11;
+            this.naruciBtn.Text = "Narucivanje";
+            this.naruciBtn.UseVisualStyleBackColor = true;
+            // 
+            // djelatniciBtn
+            // 
+            this.djelatniciBtn.Location = new System.Drawing.Point(36, 24);
+            this.djelatniciBtn.Name = "djelatniciBtn";
+            this.djelatniciBtn.Size = new System.Drawing.Size(162, 49);
+            this.djelatniciBtn.TabIndex = 12;
+            this.djelatniciBtn.Text = "Djelatnici";
+            this.djelatniciBtn.UseVisualStyleBackColor = true;
+            // 
+            // porukeBtn
+            // 
+            this.porukeBtn.Location = new System.Drawing.Point(36, 447);
+            this.porukeBtn.Name = "porukeBtn";
+            this.porukeBtn.Size = new System.Drawing.Size(162, 49);
+            this.porukeBtn.TabIndex = 13;
+            this.porukeBtn.Text = "Poruke";
+            this.porukeBtn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Dobavljac:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(25, 49);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(326, 100);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(129, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(142, 21);
+            this.comboBox1.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Datum:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dodajBtn);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.comboBox2);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(357, 49);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(326, 152);
+            this.groupBox3.TabIndex = 18;
+            this.groupBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Kolicina:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Sirovina:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(132, 34);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 18;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(132, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 19;
+            // 
+            // dodajBtn
+            // 
+            this.dodajBtn.Location = new System.Drawing.Point(132, 103);
+            this.dodajBtn.Name = "dodajBtn";
+            this.dodajBtn.Size = new System.Drawing.Size(75, 23);
+            this.dodajBtn.TabIndex = 20;
+            this.dodajBtn.Text = "Dodaj";
+            this.dodajBtn.UseVisualStyleBackColor = true;
+            // 
+            // popisSirovina
+            // 
+            this.popisSirovina.FormattingEnabled = true;
+            this.popisSirovina.Location = new System.Drawing.Point(25, 220);
+            this.popisSirovina.Name = "popisSirovina";
+            this.popisSirovina.Size = new System.Drawing.Size(658, 264);
+            this.popisSirovina.TabIndex = 14;
+            // 
+            // kreirajBtn
+            // 
+            this.kreirajBtn.Location = new System.Drawing.Point(297, 503);
+            this.kreirajBtn.Name = "kreirajBtn";
+            this.kreirajBtn.Size = new System.Drawing.Size(151, 23);
+            this.kreirajBtn.TabIndex = 14;
+            this.kreirajBtn.Text = "Kreiraj";
+            this.kreirajBtn.UseVisualStyleBackColor = true;
+            // 
+            // Vlasnik_narudzbenica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 584);
+            this.Controls.Add(this.porukeBtn);
+            this.Controls.Add(this.djelatniciBtn);
+            this.Controls.Add(this.naruciBtn);
+            this.Controls.Add(this.skladisteBtn);
+            this.Controls.Add(this.racuniOtpremniceBtn);
+            this.Controls.Add(this.zahtjevniceBtn);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.primkeBtn);
+            this.Name = "Vlasnik_narudzbenica";
+            this.Text = "Vlasnik_narudzbenica";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button primkeBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button zahtjevniceBtn;
+        private System.Windows.Forms.Button racuniOtpremniceBtn;
+        private System.Windows.Forms.Button skladisteBtn;
+        private System.Windows.Forms.Button naruciBtn;
+        private System.Windows.Forms.Button djelatniciBtn;
+        private System.Windows.Forms.Button porukeBtn;
+        private System.Windows.Forms.Button kreirajBtn;
+        private System.Windows.Forms.ListBox popisSirovina;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button dodajBtn;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+    }
+}
