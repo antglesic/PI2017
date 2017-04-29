0fc80138c7607deba549243c9fc2cc3289ec94ad
matkidjem <matkidjem@foi.hr>
2017-04-29 17:54:56 +0200

    Dodana forma vlasnik_dodavanje korisnika

---
+++ PI_Projekt_SiM/PI_Projekt_SiM/frmVlasnik_dodavanje_korisnika.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PI_Projekt_SiM
+{
+    public partial class Vlasnik_dodavanje_korisnika : Form
+    {
+        public Vlasnik_dodavanje_korisnika()
+        {
+            InitializeComponent();
+        }
+    }
+}
